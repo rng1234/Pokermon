78a584dd515df4c33caf0e2fd2d1bd72bc9efa48
rng1234 <imryan.ng@gmail.com>
2015-05-22 21:06:02 +0800

    0.01
    Still stuck in upstairs

---
+++ pkm.py
@@ -0,0 +1,59 @@
+items = []
+
+def enter():
+	print "\n"
+	raw_input("Press any key to continue..")
+	print "\n"
+
+def choose(a, b, c, d):
+
+	return ans
+
+
+def choose_answer(a, b, c, d, a1, b1, c1, d1):
+	print "\n"
+	print "a: %s \nb: %s\n" % (a, b),
+	print "c: %s \nd: %s\n" % (c, d)
+	
+	
+	while True:
+		ans = raw_input("What will you do?")
+		if ans == "a":
+			print a1
+			return ans
+		
+		elif ans == "b":
+			print b1
+			return ans
+			
+		elif ans == "c":
+			print c1
+			return ans
+			
+		elif ans == "d":
+			print d1
+			return ans
+			
+		else:
+			print "Wrong input, try again.\n"
+
+#Beginning
+print "\t\tWelcome to the world of Pokermon. "
+enter()
+
+print "What's your name? \n"
+name = raw_input("My name is: ")
+
+print "%s, This is a whole new world waiting for you, catch them all. " % name
+enter()
+
+#wake up in the room
+
+print "You standing in front of a TV and Nintendo."
+answer1 = choose_answer("Check the computer on the up-left conner.", "Go downstairs", "Check TV", "Go to sleep", "Hooray, you obtained a potion.",
+	"", "Thre's a pokermon match on TV", "After 24 hours sleeping, you wake up again.")
+
+if answer1 = "a"
+	items.append.(potion.1)
+
+
