8889851bba9452fd9bc06b5baf72f344e96522f8
rng1234 <imryan.ng@gmail.com>
2015-05-26 20:56:51 +0800

    0.02
    finished a simple "read and save" function

---
+++ README.md
@@ -0,0 +1,28 @@
+# Pokermon World #
+
+This is a python game based on pokemon.
+
+##Done List
+
+1. Improve saving function by creating local save file.
+
+##To do list##
+
+1. Find a way to put item list to the save file.
+
+
+
+
+
+
+
+
+# 这是一个模仿PKM的游戏
+
+##Done List
+
+1. 暂时完成简单的开始及读取进度
+
+## To do list
+1. 找办法将道具列表放过存档文件
+
--- pkm.py
+++ pkm.py
@@ -1,4 +1,24 @@
-items = []
+#coding:utf-8
+
+from os.path import exists #用于判断是否存在存档文件
+
+item_code = ["potion", "pokerball"] #道具列
+item_qty = [0, 0] #道具数量
+
+#check_save data
+
+start_or_continue = "1" #默认没有存档
+
+if exists("save.txt") == True: #如果存在save.txt, 则提示是续档
+	print "Welcome to Pockermon. \n1. Start Over \n2. Continue"
+	start_or_continue = raw_input("> ")
+
+
+if start_or_continue == "2": #读取存档第一行, 名字列
+	savedata = open("save.txt", "r")
+	name = savedata.readline()
+
+	
 
 def enter():
 	print "\n"
@@ -9,7 +29,6 @@ def choose(a, b, c, d):
 
 	return ans
 
-
 def choose_answer(a, b, c, d, a1, b1, c1, d1):
 	print "\n"
 	print "a: %s \nb: %s\n" % (a, b),
@@ -17,32 +36,55 @@ def choose_answer(a, b, c, d, a1, b1, c1, d1):
 	
 	
 	while True:
-		ans = raw_input("What will you do?")
+		ans = raw_input("What will you do? ")
+		print "\n"
+		
 		if ans == "a":
 			print a1
+			print "\n"
 			return ans
 		
 		elif ans == "b":
 			print b1
+			print "\n"
 			return ans
 			
 		elif ans == "c":
 			print c1
+			print "\n"
 			return ans
 			
 		elif ans == "d":
 			print d1
+			print "\n"
 			return ans
 			
+		elif ans == "item":
+			item_list()
+			print "\n"
+		
 		else:
 			print "Wrong input, try again.\n"
 
+			
+def item_list():
+	print "You bag contains below items:\n"
+	print item_code[0], item_qty[0]
+	print item_code[1], item_qty[1]	
+	
 #Beginning
 print "\t\tWelcome to the world of Pokermon. "
 enter()
 
-print "What's your name? \n"
-name = raw_input("My name is: ")
+
+if start_or_continue == "1":
+	print "What's your name? \n"
+	name = raw_input("My name is: ")
+	savedata = open("save.txt", "w")
+	savedata.write(name)	
+
+else:
+	print "Your name is %s." % name
 
 print "%s, This is a whole new world waiting for you, catch them all. " % name
 enter()
@@ -53,7 +95,9 @@ print "You standing in front of a TV and Nintendo."
 answer1 = choose_answer("Check the computer on the up-left conner.", "Go downstairs", "Check TV", "Go to sleep", "Hooray, you obtained a potion.",
 	"", "Thre's a pokermon match on TV", "After 24 hours sleeping, you wake up again.")
 
-if answer1 = "a"
-	items.append.(potion.1)
+if answer1 == "a":
+	item_qty[0] =+ 1
+	answer1 = choose_answer("Check the computer on the up-left conner.", "Go downstairs", "Check TV", "Go to sleep", "Hooray, you obtained a potion.",
+	"", "Thre's a pokermon match on TV", "After 24 hours sleeping, you wake up again.")
 
 
